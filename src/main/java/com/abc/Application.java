dc79071aef035952027d68008d249d4f13b9fa07
Kurenai <kurenai233@yahoo.com>
2019-01-09 23:55:59 +0800

    refactor(*): 添加SpringBoot配置

---
+++ src/main/java/com/abc/Application.java
@@ -0,0 +1,16 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 刘富鸿
+ * @create 2019-01-09 23:18
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
